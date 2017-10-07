6d8756f7fbae23a2a8f8a69619fa9f8e5521a7e4
Alejandro Avilés <omegak@gmail.com>
2017-10-07 23:04:17 +0200

    Improve style moar

--- source/components/App.jsx
+++ source/components/App.jsx
@@ -98,10 +98,10 @@ export default class App extends React.Component {
           <WeatherBox data={this.state.weather}/>
         </div>
         <div className="grid-item sidebar">
-          <div className="grid-item sidebar-item sidebar-announcements">
+          <div className="sidebar-item sidebar-announcements">
             <AnnouncementBox announcements={this.state.announcements}/>
           </div>
-          <div className="grid-item sidebar-item sidebar-twitter">
+          <div className="sidebar-item sidebar-twitter">
             <Twitter ref={(box) => {this.twitterBox = box}}/>
           </div>
         </div>
