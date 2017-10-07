a6bc30b1c281a1f7695034f32da123fe4b353dd5
lukebarnard <luke.barnard99@gmail.com>
2017-10-07 14:52:19 +0200

    Live news from the room

--- source/components/App.jsx
+++ source/components/App.jsx
@@ -1,31 +1,6 @@
 import React from 'react';
 import News from './News';
 
-const news = [ { title: 'Spain set for pro-unity rallies',
-    body: 'Thousands are expected to rally in Spain against Catalonian independence, after a disputed referendum.',
-    image: 'https://ichef.bbci.co.uk/images/ic/1024x576/p05j8tqr.jpg',
-    local: false },
-  { title: 'US braced for Hurricane Nate',
-    body: 'The hurricane barrelling towards the US Gulf coast has caused at least 23 deaths in central America.',
-    image: 'https://ichef-1.bbci.co.uk/news/1024/cpsprodpb/BA1E/production/_98164674_042211428-1.jpg',
-    local: false },
-  { title: 'Tearful reunion after Las Vegas heroics',
-    body: 'A Canadian woman reunites with the stranger who saved her after she had been shot in Las Vegas.',
-    image: 'https://ichef.bbci.co.uk/news/1024/cpsprodpb/16E5B/production/_98178739_reunion_976.jpg',
-    local: false},
-  { title: 'Trump rolls back free birth control',
-    body: 'The new rule could strip birth control coverage from millions of women, demolishing an Obama provision.',
-    image: 'https://ichef.bbci.co.uk/images/ic/1024x576/p05hdggf.jpg',
-    local: false },
-    ];
-  // { title: 'No more Shakespeare for Sir Ian McKellen?',
-  //   body: 'The actor says his current role as King Lear is likely to be his last big Shakespeare role.',
-  //   image: 'https://ichef.bbci.co.uk/images/ic/1024x576/p05j82jw.jpg',
-  //   local: false },
-  // { title: 'Ryanair\'s top operations boss resigns',
-  //   body: 'The airline will bid farewell to operations manager who had ultimate responsibility for pilot rosters',
-  //   image: 'https://ichef.bbci.co.uk/news/1024/cpsprodpb/3E88/production/_98180061_n7faahvq.jpg',
-  //   local: false } ];
 
 import AnnouncementBox from './AnnouncementBox.jsx';
 
@@ -34,9 +9,21 @@ const ROOM_ID = '!OfRBJBuhWHWNKplCtn:matrix.org';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      "news": [],
+    };
     props.client.on("event", (event) => {
-        if (event.getRoomId() == ROOM_ID && event.getType() == 'm.room.message') {
-            this.announcementBox.addMessage(event.getContent());
+        if (event.getRoomId() == ROOM_ID) {
+            switch (event.getType()) {
+              case 'm.room.message':
+                this.announcementBox.addMessage(event.getContent());
+              break;
+              case 'c.news':
+                this.setState({
+                  news: this.state.news.concat([event.getContent()]),
+                });
+              break;
+            }
         }
     });
     props.client.startClient();
@@ -50,7 +37,7 @@ export default class App extends React.Component {
             <AnnouncementBox ref={(box) => {this.announcementBox = box}}/>
           </div>
           <div className="column1-row2">
-            <News news={news} />
+            <News news={this.state.news} />
           </div>
         </div>
         <div className="column2">
--- source/components/News.js
+++ source/components/News.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Article extends React.Component {
 	render() {
-		return (<div class="article">
+		return (<div className="article">
 			<img src={this.props.image}/>
 			<h1>{this.props.title}</h1>
 			<p>{this.props.body}</p>
@@ -14,13 +14,11 @@ export default class News extends React.Component {
   render() {
 
   	// TODO: Filter on `local` key
-  	const globalNews = this.props.news;
+  	const globalNews = this.props.news.slice(-4);
 
-  	const newsItems = globalNews.map((article) => {
-  		return <Article title={article.title} body={article.body} image={article.image}/>;
+  	const newsItems = globalNews.map((article, index) => {
+  		return <Article key={index} title={article.title} body={article.body} image={article.image}/>;
   	});
-
-  	console.info(newsItems);
 
     return (
       <div className="news">
