52f10ca32202c9a9f604c218453828f5d59f2fff
lukebarnard <luke.barnard99@gmail.com>
2017-10-07 15:08:58 +0200

    Adjust news component, add header

--- source/components/News.js
+++ source/components/News.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
 class Article extends React.Component {
-	render() {
-		return (<div className="article">
-			<img src={this.props.image}/>
-			<h1>{this.props.title}</h1>
-			<p>{this.props.body}</p>
-		</div>);
-	}
+  render() {
+    return (<div className="article">
+      <img src={this.props.image}/>
+      <h1>{this.props.title}</h1>
+      <p>{this.props.body}</p>
+    </div>);
+  }
 }
 
 export default class News extends React.Component {
   render() {
 
-  	// TODO: Filter on `local` key
-  	const globalNews = this.props.news.slice(-4);
+    // TODO: Filter on `local` key
+    const globalNews = this.props.news.slice(-4);
 
-  	const newsItems = globalNews.map((article, index) => {
-  		return <Article key={index} title={article.title} body={article.body} image={article.image}/>;
-  	});
+    const newsItems = globalNews.map((article, index) => {
+      return <Article key={index} title={article.title} body={article.body} image={article.image}/>;
+    });
 
-    return (
+    return (<div>
+      <h1 className="newsHeader">News</h1>
       <div className="news">
-      	{newsItems}
-      </div>);
+          {newsItems}
+      </div>
+    </div>);
   }
 }
