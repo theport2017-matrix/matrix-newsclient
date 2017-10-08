e7d3928c31df791e3884f7f354e1cfa0d31157cd
Pedro Ferreira <pedro@dete.st>
2017-10-08 14:07:53 +0200

    Ignore announcements older than 5h

--- source/components/App.jsx
+++ source/components/App.jsx
@@ -10,6 +10,7 @@ import WeatherBox from './Weather.jsx';
 
 const ROOM_ID = '!OfRBJBuhWHWNKplCtn:matrix.org';
 const TWITTER_ROOM_ID = '!kgfNoSRLkBFxmVGvxw:matrix.org';
+const MAX_ANNOUNCEMENT_AGE = 5; // hours
 
 let backPaginated = false;
 
@@ -34,7 +35,7 @@ export default class App extends React.Component {
             switch (event.getType()) {
               case 'm.room.message':
                 const ts = moment(event.getTs());
-                if (moment().diff(ts, 'hours') > 1) {
+                if (moment().diff(ts, 'hours') > MAX_ANNOUNCEMENT_AGE) {
                   // exclude announcements older than 1h
                   return;
                 }
